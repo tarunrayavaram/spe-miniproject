23e0a793bab01a9543d9489436c51dd9c147aad1
tarunrayavaram <tarunrayavaram@gmail.com>
2022-03-22 14:49:55 +0530

    resolved test

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -7,16 +7,17 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest
 {
+	private static final double DELTA = 1e-15;
 	@SuppressWarnings("deprecation")
 	@Test
 	public void square_root_test()
 	{
-		assertEquals(5.0, calculator.square_root(25));
+		assertEquals(5.0, calculator.square_root(25), DELTA);
 	}
 	
 	@Test
 	public void factorial_test()
 	{
-		assertEquals(120, calculator.factorial(5));
+		assertEquals(120, calculator.factorial(5), DELTA);
 	}
 }
