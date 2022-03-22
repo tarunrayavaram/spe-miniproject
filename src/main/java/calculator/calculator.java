215257b354ea45d64a95280715db7e70711d2cfc
Tarun Rayavaram <44033028+tarunrayavaram@users.noreply.github.com>
2022-03-22 12:56:06 +0530

    Add files via upload

---
+++ src/main/java/calculator/calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class calculator
+{
+	public static double square_root(Integer a)
+	{
+		double sqr = Math.sqrt(a);
+		return sqr;
+	}
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import calculator.calculator;
+import java.io.*;
+
+class main
+{
+	public static void main(String[] args)
+	{
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Choose the given options. Press the integers based on the option to choose");
+		System.out.println("1) Square Root");
+		int choice = scn.nextInt();
+		if(choice == 1)
+		{
+			System.out.println("type the integer:");
+			int num = scn.nextInt();
+			double result = calculator.square_root(num);
+			System.out.println("square root of "+num+" is:"+result);
+		}
+	}
+}
